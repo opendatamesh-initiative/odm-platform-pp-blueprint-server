2fd69d09d88fe1404bcfd0429832a2f721f34e67
Andrea Carubelli <andrea.carubelli@blindata.io>
2026-04-03 19:33:53 +0200

    fix: tagMapper mapping removed

--- src/main/java/org/opendatamesh/platform/pp/blueprint/rest/v2/resources/gitproviders/TagMapper.java
+++ src/main/java/org/opendatamesh/platform/pp/blueprint/rest/v2/resources/gitproviders/TagMapper.java
@@ -1,11 +1,9 @@
 package org.opendatamesh.platform.pp.blueprint.rest.v2.resources.gitproviders;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.opendatamesh.platform.git.model.Tag;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
-    @Mapping(source = "author", target = "authorName")
     TagRes toRes(Tag tag);
 }
